97f748dc14ce49cd2148cb7fe4e7e6e8633c1c8e
daninisc <dpsantandercastro@gmail.com>
2018-06-19 14:25:09 -0400

    fix: conexión

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ mongoose.connect('mongodb://localhost:27017/curso_mean2', (err, res) => {
     if(err){
         throw err;
     } else{
-        console.log("La coneccion a la base de datos esta funcionando correctamente :3");
+        console.log("La conexión a la base de datos está funcionando correctamente.");
         
         app.listen(port, function() {
-            console.log("Servidor del API Rest de musica escuchando en http://localhost:" + port);
+            console.log("Servidor del API Rest de música escuchando en http://localhost:" + port);
         });
     }
 });
